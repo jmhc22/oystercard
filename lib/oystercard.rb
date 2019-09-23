f55027d6087b7bdf5f578d2a79b9a6bdf266a947
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-23 15:15:25 +0100

    Adds function to touch in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,35 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
   MAXIMUM_LIMIT = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(value)
-    raise "Error: over maximum limit" if @balance + value > MAXIMUM_LIMIT
+    raise "Error: £#{MAXIMUM_LIMIT} exceeded" if @balance + value > MAXIMUM_LIMIT
     @balance += value
   end
 
+  def deduct(value)
+    @balance -= value
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise "Error: already on journey" if in_journey?
+
+    @in_journey = true
+  end
+
+  def touch_out
+    raise "Error: not currently in use" if !in_journey?
+
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,10 +17,44 @@ describe Oystercard do
     end
 
     it 'sets a maximum limit of 90' do
-      oystercard.top_up(Oystercard::MAXIMUM_LIMIT)
-      expect{ oystercard.top_up(1) }.to raise_error("Error: over maximum limit")
+      maximum_balance = Oystercard::MAXIMUM_LIMIT
+      oystercard.top_up(maximum_balance)
+      expect{ oystercard.top_up(1) }.to raise_error("Error: £#{maximum_balance} exceeded")
     end
+  end
 
+  describe '#deduct' do
+    it 'deducts £10 from the oystercard balance' do
+      oystercard.top_up(50)
+      oystercard.deduct(10)
+      expect(oystercard.balance).to eq 40
+    end
+  end
+
+  describe '#in_journey?' do
+    it { expect(oystercard.in_journey).to eq false }
+  end
+
+  describe 'touch_in' do
+    it 'starts journey' do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+    it 'does not start journey if already on one' do
+      oystercard.touch_in
+      expect { oystercard.touch_in }.to raise_error("Error: already on journey")
+    end
+  end
+
+  describe 'touch_out' do
+    it 'ends journey' do
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard).to_not be_in_journey
+    end
+    it 'does not end a journey if not currently on one' do
+      expect { oystercard.touch_out }.to raise_error("Error: not currently in use")
+    end
   end
 
 end
