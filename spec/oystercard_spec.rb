fa127765a02c51dbba6d769cfcea46f5d4404d4a
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-09-23 14:18:25 +0100

    Adds default balance to initialized Oystercard objects

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'Oystercard'
+
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+  describe '#balance' do
+    it { expect(oystercard.balance).to eq 0 }
+  end
 
 end
